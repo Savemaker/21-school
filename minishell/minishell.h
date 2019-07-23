5be5098fe6fbdd1e7d6de10712ebe0e3eccf174d
Gurn Beqqo <gbeqqo@il-e4.21-school.ru>
2019-07-23 19:24:13 +0300

    shell

---
+++ minishell/minishell.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbeqqo <gbeqqo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/23 16:47:55 by gbeqqo            #+#    #+#             */
+/*   Updated: 2019/07/23 17:08:01 by gbeqqo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include "libft/includes/libft.h"
+# include <dirent.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <signal.h>
+#endif
